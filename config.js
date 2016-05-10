5ae3a3a40469195183e165b9a2ad8df0ac0d3ad9
Aman Mittal <amandeepmittal@live.com>
2016-05-10 19:52:47 +0530

    commit: config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+/*
+* TWITTER APP CONFIGURATION
+* consumer_key
+* consumer_secret
+* access_token
+* access_token_secret
+*/
+module.exports = {
+  consumer_key       : 'Cbbp9QbkU0zzOfqOFofTsYtiY',
+  consumer_secret    : 'kDWopgD78RjIvHqRBi0KVUxj9FozKfPu8yCj6mb9UnVpeuC481',
+  access_token       : '729923589978251266-cDPAFDCGGoNaY6kVUkpTqt3Q9vysv3C',
+  access_token_secret: 'g5KTz7lIHdmGcnuzghwB70NiTb6qZRA6b4v231i3LIr8Y'
+}
