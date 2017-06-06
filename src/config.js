6759e58104a55559ee85208495f1d9ab4e845924
amandeepmittal <amandeepmittal@live.com>
2017-06-06 20:23:17 +0530

    update config file

---
+++ src/config.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/*
+* TWITTER APP CONFIGURATION
+* consumer_key
+* consumer_secret
+* access_token
+* access_token_secret
+* retweet frequency rate
+* favorite frequency rate
+*/
+module.exports = {
+  twitter: {
+    consumer_key       : 'eqS99PFooAF7KulehNv7NLP36',
+    consumer_secret    : 'vwPH96APug5U0K3LWNK1unwoASqBy7G63lXzOPnweix4dFcnYO',
+    access_token       : '729923589978251266-cDPAFDCGGoNaY6kVUkpTqt3Q9vysv3C',
+    access_token_secret: 'g5KTz7lIHdmGcnuzghwB70NiTb6qZRA6b4v231i3LIr8Y'
+  },
+  retweetRate: 1500000,
+  favoriteRate: 1500000
+}
